2a9d6b97923444614f883835937a20715df92520
Devansh-kajve <devanshkajve123@gmail.com>
2023-02-27 23:47:27 +0530

    Added api endpoint to fetch NFTs

--- build/index.js
+++ build/index.js
@@ -14,13 +14,20 @@ const parse_server_1 = __importDefault(require("parse-server"));
 const http_1 = __importDefault(require("http"));
 const ngrok_1 = __importDefault(require("ngrok"));
 const parse_server_2 = require("@moralisweb3/parse-server");
+const { EvmChain } = require("@moralisweb3/common-evm-utils");
 exports.app = (0, express_1.default)();
+const port = 4000;
+const address = "0x45F0bF42fc26923e88a46b15Ad22B89fA50Dbb37";
+const chain = 5;
 moralis_1.default.start({
     apiKey: config_1.default.MORALIS_API_KEY,
 });
 exports.app.use(express_1.default.urlencoded({ extended: true }));
 exports.app.use(express_1.default.json());
-exports.app.use((0, cors_1.default)());
+exports.app.use((0, cors_1.default)({
+    origin: 'http://localhost:3000',
+    credentials: true,
+}));
 exports.app.use((0, parse_server_2.streamsSync)(parseServer_1.parseServer, {
     apiKey: config_1.default.MORALIS_API_KEY,
     webhookUrl: '/streams',
@@ -40,4 +47,36 @@ httpServer.listen(config_1.default.PORT, async () => {
 });
 // This will enable the Live Query real-time server
 parse_server_1.default.createLiveQueryServer(httpServer);
+async function getDemoData() {
+    // Get the nfts
+    const nftsBalances = await moralis_1.default.EvmApi.nft.getWalletNFTs({
+        address,
+        chain,
+        limit: 10,
+    });
+    // Format the output to return name, amount and metadata
+    const nfts = nftsBalances.result.map((nft) => ({
+        metadata: nft.result.metadata,
+        token_address: nft.tokenAddress,
+        token_id: nft.tokenId,
+    }));
+    return { nfts };
+}
+exports.app.get("/demo", async (req, res) => {
+    try {
+        // Get and return the crypto data
+        const data = await getDemoData();
+        res.status(200);
+        res.json(data);
+    }
+    catch (error) {
+        // Handle errors
+        console.error(error);
+        res.status(500);
+        res.json({ error: error.message });
+    }
+});
+exports.app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,14 @@ import ParseServer from 'parse-server';
 import http from 'http';
 import ngrok from 'ngrok';
 import { streamsSync } from '@moralisweb3/parse-server';
-// const { EvmChain } = require("@moralisweb3/common-evm-utils")
+const { EvmChain } = require("@moralisweb3/common-evm-utils")
 
 export const app = express();
 
-// const address = "0x45F0bF42fc26923e88a46b15Ad22B89fA50Dbb37"
-// const chain = EvmChain.ETHEREUM
+const port = 4000
+
+const address = "0x45F0bF42fc26923e88a46b15Ad22B89fA50Dbb37"
+const chain = 5
 
 Moralis.start({
   apiKey: config.MORALIS_API_KEY,
@@ -22,7 +24,10 @@ Moralis.start({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}));
 
 app.use(
   streamsSync(parseServer, {
@@ -49,35 +54,40 @@ httpServer.listen(config.PORT, async () => {
 // This will enable the Live Query real-time server
 ParseServer.createLiveQueryServer(httpServer);
 
-// async function getDemoData() {
-//   // Get the nfts
-//   const nftsBalances = await Moralis.EvmApi.nft.getWalletNFTs({
-//     address,
-//     chain,
-//     limit: 10,
-//   })
+async function getDemoData() {
+  // Get the nfts
+  const nftsBalances = await Moralis.EvmApi.nft.getWalletNFTs({
+    address,
+    chain,
+    limit: 10,
+  })
 
-//   // Format the output to return name, amount and metadata
-//   const nfts = nftsBalances.result.map((nft) => ({
-//     name: nft.result.name,
-//     amount: nft.result.amount,
-//     metadata: nft.result.metadata,
-//   }))
+  // Format the output to return name, amount and metadata
+  const nfts = nftsBalances.result.map((nft) => ({
+    metadata: nft.result.metadata,
+    token_address: nft.tokenAddress,
+    token_id: nft.tokenId,
+  }))
 
-//   return { nfts }
-// }
+  return { nfts }
+}
 
-// app.get("/demo", async (req, res) => {
-//   try {
+app.get("/demo", async (req, res) => {
+  try {
 
-//     // Get and return the crypto data
-//     const data = await getDemoData()
-//     res.status(200)
-//     res.json(data)
-//   } catch (error) {
-//     // Handle errors
-//     console.error(error)
-//     res.status(500)
-//     res.json({ error: error.message })
-//   }
-// })
+    // Get and return the crypto data
+    const data = await getDemoData()
+    res.status(200)
+    res.json(data)
+    
+  } catch (error) {
+    // Handle errors
+    console.error(error)
+    res.status(500)
+    res.json({ error: error.message })
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
